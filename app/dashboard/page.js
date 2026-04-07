a55e2d84f241c5e073a408bc4e6a4c2f7d99ecdb
Akash bauri <108834431+akashbauri@users.noreply.github.com>
2026-04-08 01:01:41 +0530

    Create page.js

---
+++ app/dashboard/page.js
@@ -0,0 +1,12 @@
+import Navbar from "../../components/Navbar";
+
+export default function Page() {
+  return (
+    <>
+      <Navbar />
+      <div style={{ padding: "40px", textAlign: "center" }}>
+        <h2>Page</h2>
+      </div>
+    </>
+  );
+}
